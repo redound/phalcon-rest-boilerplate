1f0f98c780b83c70d7fe41ba644676aba3cec6ca
Olivier Andriessen <olivierandriessen@gmail.com>
2015-12-25 12:41:07 +0100

    Update comments

--- app/bootstrap/autoload.php
+++ app/bootstrap/autoload.php
@@ -8,9 +8,7 @@ define('PHALCON_REST_START', microtime(true));
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
+| for our application.
 |
 */
 
