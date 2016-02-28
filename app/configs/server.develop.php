4f94d990077ab18eb83412f328d24a0c9f8458a5
Olivier Andriessen <olivierandriessen@gmail.com>
2016-02-28 12:37:26 +0100

    Develop server config: add

---
+++ app/configs/server.develop.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'debug' => true,
+    'hostName' => 'http://phalcon-rest-boilerplate.redound.dev',
+    'clientHostName' => 'http://phalcon-rest-boilerplate.redound.dev',
+    'database' => [
+
+        // Change to your own configuration
+        'adapter' => 'Mysql',
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+        'name' => 'phalcon_rest_boilerplate',
+    ],
+    'cors' => [
+        'allowedOrigins' => ['phalcon-rest-boilerplate.dev']
+    ]
+];
