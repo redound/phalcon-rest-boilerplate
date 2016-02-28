f356800e3d9b24dda3c519b59a393aeda997926a
Olivier Andriessen <olivierandriessen@gmail.com>
2016-02-28 11:10:13 +0100

    Config: tidy

--- app/configs/default.php
+++ app/configs/default.php
@@ -12,22 +12,5 @@ return [
     'authentication' => [
         'secret' => 'this_should_be_changed',
         'expirationTime' => 86400 * 7, // One week till token expires
-    ],
-    'acl' => [
-        'publicEndpoints' => [
-            '/',
-            '/proxy.html',
-            '/export/documentation.json',
-            '/export/postman.json',
-            '/users',
-            '/users/authenticate'
-        ],
-        'privateEndpoints' => [
-            '/users/me',
-            '/products',
-            '/products/{product_id}',
-            '/{resourceKey}',
-            '/{resourceKey}/{id}',
-        ],
     ]
 ];
